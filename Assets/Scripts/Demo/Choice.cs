df8ef76e1caac36e82140808f1b54e2f7f1be0ea
Carson Angelle <carsonangelle19@tchs.net>
2019-02-21 21:03:20 -0600

    Add files via upload
    Dynamic Prompt System, Version 1

--- Assets/Scripts/Demo/Choice.cs
+++ Assets/Scripts/Demo/Choice.cs
@@ -1,41 +1,20 @@
 ﻿using System;
-namespace LifeTest
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Engine
 {
-    //making this abstract means that every new choice we make will need to first be 
-    //initialized by us, which wouldn't be too hard, considering the abstract does
-    //most of the work, we put in the creativity, but it is something to keep in mind
-    //this is where the list of choices would come in handy
+    public struct Choice
+    {
+        public string text;
+        public Action action;
 
-    public abstract class Choice
-    {  
-        public string prompt;   
-        //not sure if this part will be kept, may just make everything a value check
-        public Action choiceAction;
-
-
-        public void ValueCheck(int minimumPtsToPass, int ptValueChecking) 
+        public Choice(string choiceText, Action choiceAction)
         {
-            if (ptValueChecking >= minimumPtsToPass)
-            {
-                Success();
-            }
-            else {
-                Failed();
-            }
+            text = choiceText;
+            action = choiceAction;
         }
-        public virtual void Success() { 
-            //whatever happens if the player succeeds the skillCheck goes here
-        }
-        public virtual void Failed() { 
-            //whatever happens if they fail goes here
-        }
-
-        public Choice(string v, Action a)
-            {
-                prompt = v;
-                choiceAction = a;
-                
-            }
-        }
-
+    }
 }
---
+++ Assets/Scripts/Demo/Prompt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame.Engine
+{
+    public class Prompt
+    {
+        public string text;
+        public List<Choice> choices = new List<Choice>();
+
+        public Prompt(string promptText)
+        {
+            text = promptText;
+        }
+
+        public Prompt(string promptText, List<Choice> promptChoices)
+        {
+            text = promptText;
+            choices = promptChoices;
+        }
+
+        public static void RunPrompt(Prompt prompt)
+        {
+            Console.WriteLine(prompt.text);
+            for (int i = 0; i < prompt.choices.Count; i++)
+            {
+                Console.WriteLine("[" + (i+1) + "] " + prompt.choices[i].text);
+            }
+            Console.WriteLine();
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (input)
+            {
+                case int i when (i >= 0 && i <= prompt.choices.Count):
+                    prompt.choices[i-1].action();
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
